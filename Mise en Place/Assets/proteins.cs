3774107caed7c52d707c76234023ae7b5648dca2
sspangler <sspangler@ltu.edu>
2016-11-03 16:52:29 -0400

    All objects with the script Proteins will, when put on the oven start to change color from their starting color to black. just set the burnt time variable and the rest is taken care of automatically

---
+++ Mise en Place/Assets/proteins.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class proteins : MonoBehaviour {
+
+	public bool cooking;
+	public float timeToCook;
+	public float cookedTime;
+	public float burntTime;
+
+	public Color startColor;
+	public Color cookedColor;
+	public Color burntColor = Color.black;
+
+	MeshRenderer foodColor;
+	// Use this for initialization
+	void Start () {
+		foodColor = GetComponent<MeshRenderer> ();
+		startColor = foodColor.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cooking) {
+			cookedTime += Time.deltaTime;
+			foodColor.material.color = Color.Lerp (startColor, burntColor, cookedTime / burntTime); //lerps between the starting color and black
+		}
+	}
+
+	void OnTriggerEnter (Collider col) {
+		if (col.name == "Oven") {
+			cooking = true;
+		}
+	}
+
+	void OnTriggerExit (Collider col) {
+		if (col.name == "Oven") {
+			cooking = false;
+		}
+	}
+}
